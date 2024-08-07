737018875774a429896d050aa39647e31a3c2ea6
kodzimk <proggamedev@gmail.com>
2024-08-08 00:23:01 +0500

    testing an refactoring

--- out.asm
+++ out.asm
@@ -1,22 +1,22 @@
 global _start
 _start:
-   mov rax,2
+   mov rax,5
    push rax
-   push QWORD [rsp + 0]
-
-   mov rax,10
+   mov rax,5
+   push rax
+   mov rax,2
    push rax
    mov rax,15
    push rax
     pop rax
     pop rbx
-    sub rax, rbx
-   push rax
-   mov rax,10
+    mul rbx
    push rax
     pop rax
     pop rbx
-    add rax, rbx
+    div rbx
+   push rax
+   mov rax,10
    push rax
     pop rax
     pop rbx
