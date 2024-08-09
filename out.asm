75fcc929d46ee9d742251fb54fc609f7d5ed8ab2
kodzimk <proggamedev@gmail.com>
2024-08-10 00:04:50 +0500

    i am genius

--- out.asm
+++ out.asm
@@ -4,11 +4,13 @@ symbol db 's'
 section .text
 global _start
 _start:
-    mov edx,1
-    mov ecx,symbol
-    mov ebx,1
-    mov eax,4
-    int 0x80
+    mov rax,[symbol]
+    mov [symbol], dword '5'
+    jmp printf
+
+    mov [symbol],rax
+   jmp printf
+
    mov rax,1
    push rax
    push QWORD [rsp + 0]
@@ -16,3 +18,10 @@ _start:
     mov rax,60
     pop rdi
     syscall
+
+printf:
+    mov edx,1
+    mov ecx,symbol
+    mov ebx,1
+    mov eax,4
+    int 0x80
